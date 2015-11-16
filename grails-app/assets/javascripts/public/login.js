5be7ad3d491f325a73c15cd89ddecf1553b69762
Federico <fcatinello@gmail.com>
2015-11-16 15:00:25 -0300

    Bugfixed in login js

--- grails-app/assets/javascripts/public/login.js
+++ grails-app/assets/javascripts/public/login.js
@@ -3,20 +3,25 @@ angular.module('myApp')
 
     $scope.user = "";
     $scope.name = "";
-    $scope.pass = "";
 
     $scope.storeData = function() {
-        /* Putting user credentials in session storage */
         sessionStorage.setItem("username", $scope.user);
     };
 
     $scope.searchName = function() {
+
+        if (sessionStorage.getItem("name") === null) {
+            $http.get('/person/show/' + $scope.user).then(function(response){
+                var user = response.data;
+                $scope.name = user.name + ' ' + user.lastname;
+                sessionStorage.setItem("name", $scope.name);
+            }, function () {
+
+            });
+        }
+
         $scope.user = sessionStorage.getItem("username");
-        $http.get('/person/show/'+$scope.user).then(function(response){
-            var user = response.data;
-            $scope.name = user.name+' '+user.lastname;
-            sessionStorage.setItem("name", $scope.name);
-        });
+        $scope.name = sessionStorage.getItem("name");
     };
 
     $scope.logOut = function() {
