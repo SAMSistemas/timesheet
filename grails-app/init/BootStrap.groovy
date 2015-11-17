4e7ed9cdc4b31a5c6d6cf26e09141577ea6b262e
Mariano <mloisotto@gmail.com>
2015-11-17 11:00:46 -0300

    Add workpositions to app - Bug #10

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -5,7 +5,14 @@ import sam.timesheet.domain.WorkPosition
 class BootStrap {
 
     def init = { servletContext ->
-        def project_manager = (new WorkPosition(description: "project manager")).save flush: true
+
+        def project_manager = (new WorkPosition(description: "Project Manager")).save flush: true
+        def developer = (new WorkPosition(description: "Desarrollador")).save flush: true
+        def tester = (new WorkPosition(description: "Tester")).save flush: true
+        def functional_analyst = (new WorkPosition(description: "Analista Funcional")).save flush: true
+        def technical_leader = (new WorkPosition(description: "Lider Tecnico")).save flush: true
+        def architect = (new WorkPosition(description: "Arquitecto")).save flush: true
+
         def person1 = (new Person(name:"admin", lastname:"Administrador", username: "admin", password: "AAAAAAAA", picture: null, work_position: project_manager, enabled: true)).save flush: true
 
         def springContext = WebApplicationContextUtils.getWebApplicationContext( servletContext )
