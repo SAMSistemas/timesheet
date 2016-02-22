36b292b5e3a5be20544262c5d130a1fd0f925e4e
Santiago <santiago.pereztorre@gmail.com>
2016-02-22 14:28:49 -0300

    Update HolidayController to use RestfulController

--- grails-app/controllers/sam/timesheet/HolidayController.groovy
+++ grails-app/controllers/sam/timesheet/HolidayController.groovy
@@ -1,162 +1,59 @@
 package sam.timesheet
 
-import grails.converters.JSON
+import grails.rest.RestfulController
 
 import java.text.ParseException
 import java.text.SimpleDateFormat
 
-import static org.springframework.http.HttpStatus.*
-import grails.transaction.Transactional
+class HolidayController extends RestfulController {
 
-@Transactional(readOnly = true)
-class HolidayController {
+    static responseFormats = ['json']
 
-    static allowedMethods = [all: "GET", show: "GET", create: "POST", update: "PUT", delete: "DELETE"]
-
-    def index() {}
-
-    def all() {
-        render(contentType: "application/json") {
-            array {
-                for (h in Holiday.list()) {
-                    holiday (
-                            id: h.id,
-                            title: h.description,
-                            start: h.holiday_date.format("yyyy-MM-dd"),
-                    )
-                }
-            }
-        }
+    HolidayController() {
+        super(Holiday)
     }
 
-    def show() {
+    @Override
+    def index() {
+        def filters = [:]
 
-        def holiday = Holiday.findById(params.id)
+        if (params.title)
+            filters.put("title", params.title)
 
-        if (holiday == null) {
-
-            response.status = 404
-
-            render(contentType: "application/json") {
-                error = "El feriado no existe"
-            }
-
-            return
-        }
-
-        render holiday as JSON
-
+        if (filters.isEmpty())
+            respond Holiday.list()
+        else
+            respond Holiday.findAllWhere(filters)
     }
 
-    def formatDate(dateInString) {
+    @Override
+    protected getObjectToBind() {
+        def paramsJSON = request.JSON
+        def params = [:]
 
-        def formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        if (paramsJSON.title)
+            params.put("title", paramsJSON.title)
 
+        if (paramsJSON.date)
+            params.put("date", formatDate(paramsJSON.date))
+
+        params
+    }
+
+    @Override
+    protected Object queryForResource(Serializable query) {
+        if (query.toString().isNumber())
+            Holiday.findById(query)
+        else
+            Holiday.findByName(query)
+    }
+
+    def formatDate(String dateInString) {
         try {
-
-            def date = formatter.parse(dateInString)
-            return date
-
+            new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(dateInString)
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
-    }
-
-    @Transactional
-    def create() {
-
-        def paramsJSON = request.JSON
-
-        def newHolidayParams = [
-                description: paramsJSON.get("title"),
-                holiday_date: formatDate(paramsJSON.get("start"))
-        ]
-
-        def newHoliday = new Holiday(newHolidayParams)
-
-        if (!newHoliday.validate()) {
-
-            response.status = 422
-
-            render newHoliday.errors.fieldErrors as JSON
-
-            return
-        }
-
-        newHoliday.save flush: true
-
-        render newHoliday as JSON
-
-    }
-
-
-    @Transactional
-    def update() {
-
-        def paramsJSON = request.JSON
-
-        def holidayToUpdate = Holiday.findById(paramsJSON.get("id"))
-
-        if (holidayToUpdate == null) {
-
-            response.status = 404
-
-            render(contentType: "application/json") {
-                error = "El feriado no existe"
-            }
-
-            return
-        }
-
-        holidayToUpdate.description = paramsJSON.get("title")
-        holidayToUpdate.holiday_date = formatDate(paramsJSON.get("start"))
-
-        if (!holidayToUpdate.validate()) {
-
-            response.status = 422
-
-            render holidayToUpdate.errors.fieldErrors as JSON
-
-            return
-        }
-
-        holidayToUpdate.save flush: true
-
-        render holidayToUpdate as JSON
-
-    }
-
-
-    @Transactional
-    def delete() {
-
-        def holidayToDelete = Holiday.findById(params.id)
-
-        if (holidayToDelete == null) {
-
-            response.status = 404
-
-            render(contentType: "application/json") {
-                error = "El feriado no existe"
-            }
-
-            return
-        }
-
-        if (!holidayToDelete.validate()) {
-
-            response.status = 422
-
-            render holidayToDelete.errors.fieldErrors as JSON
-
-            return
-        }
-
-        holidayToDelete.delete flush: true
-
-        render OK
-
     }
 
 }
