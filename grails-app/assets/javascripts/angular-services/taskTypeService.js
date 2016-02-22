c31c5f86d8532bcb9b954a612e5fb085090074eb
Santiago <santiago.pereztorre@gmail.com>
2016-02-22 13:56:17 -0300

    Update taskTypeService to new task type api

--- grails-app/assets/javascripts/angular-services/taskTypeService.js
+++ grails-app/assets/javascripts/angular-services/taskTypeService.js
@@ -6,15 +6,15 @@ function taskTypeService($http) {
     var vm = this;
 
     vm.getTaskTypes = function(callbackSuccess, callbackFailure){
-        $http.get('/taskType/all').then(callbackSuccess, callbackFailure);
+        $http.get('/taskTypes').then(callbackSuccess, callbackFailure);
     };
 
     vm.createTaskType = function(taskTypeToCreate,callbackSuccess, callbackFailure){
-        $http.post('/taskType/create', taskTypeToCreate).then(callbackSuccess, callbackFailure);
+        $http.post('/taskTypes', taskTypeToCreate).then(callbackSuccess, callbackFailure);
     };
 
     vm.updateTaskType = function(taskTypeToEdit, callbackSuccess, callbackFailure){
-        $http.put('/taskType/update', taskTypeToEdit).then(callbackSuccess, callbackFailure);
+        $http.put('/taskTypes/' + taskTypeToEdit.id, taskTypeToEdit).then(callbackSuccess, callbackFailure);
     };
 
 }
