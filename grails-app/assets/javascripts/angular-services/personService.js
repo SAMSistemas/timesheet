542f7af44fef0e249e5a946a6b053224fc37270e
Santiago <santiago.pereztorre@gmail.com>
2016-02-22 14:57:49 -0300

    Update personService to new person api

--- grails-app/assets/javascripts/angular-services/personService.js
+++ grails-app/assets/javascripts/angular-services/personService.js
@@ -6,23 +6,23 @@ function personService($http) {
     var vm = this;
 
     vm.showPerson = function(username, callbackSuccess, callbackFailure){
-        $http.get('/person/show/' + username).then(callbackSuccess, callbackFailure);
+        $http.get('/people?username=' + username).then(callbackSuccess, callbackFailure);
     };
 
     vm.getPersonAvailableForProject = function(projectName, callbackSuccess, callbackFailure) {
-        $http.get('/person/allEnabledAndAvailableForProject/' + projectName).then(callbackSuccess, callbackFailure);
+        $http.get('/people?availableFor=' + projectName).then(callbackSuccess, callbackFailure);
     };
 
     vm.getPeople = function(callbackSuccess, callbackFailure){
-        $http.get('/person/all').then(callbackSuccess, callbackFailure);
+        $http.get('/people').then(callbackSuccess, callbackFailure);
     };
 
     vm.createPerson = function(personToCreate,callbackSuccess, callbackFailure){
-        $http.post('/person/create', personToCreate).then(callbackSuccess, callbackFailure);
+        $http.post('/people', personToCreate).then(callbackSuccess, callbackFailure);
     };
 
     vm.updatePerson = function(personToEdit, callbackSuccess, callbackFailure){
-        $http.put('/person/update', personToEdit).then(callbackSuccess, callbackFailure);
+        $http.put('/people/' + personToEdit.id, personToEdit).then(callbackSuccess, callbackFailure);
     };
 
 }
