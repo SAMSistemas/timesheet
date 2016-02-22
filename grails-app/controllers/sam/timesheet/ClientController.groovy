d5cf0d0721d27d8fdf35afdefd4d281910560297
Santiago <santiago.pereztorre@gmail.com>
2016-02-22 14:35:03 -0300

    Update ClientController to use RestfulController

--- grails-app/controllers/sam/timesheet/ClientController.groovy
+++ grails-app/controllers/sam/timesheet/ClientController.groovy
@@ -1,140 +1,42 @@
 package sam.timesheet
 
-import grails.converters.JSON
-
+import grails.rest.RestfulController
 import grails.transaction.Transactional
 
 @Transactional(readOnly = true)
-class ClientController {
+class ClientController extends RestfulController {
 
-    static allowedMethods = [all: "GET", allEnabled: "GET", show: "GET", existsName: "GET", existsSName: "GET", create: "POST", update: "PUT"]
+    static responseFormats = ['json']
 
-    def index() {}
-
-    def all() {
-        render Client.list() as JSON
+    ClientController() {
+        super(Client)
     }
 
-    def allEnabled() {
-        render Client.findAllByEnabled(true) as JSON
+    @Override
+    def index() {
+        def filters = [:]
+
+        if (params.name)
+            filters.put("name", params.name)
+
+        if (params.short_name)
+            filters.put("short_name", params.short_name)
+
+        if (params.enabled)
+            filters.put("enabled", Boolean.parseBoolean(params.enabled))
+
+        if (filters.isEmpty())
+            respond Client.list()
+        else
+            respond Client.findAllWhere(filters)
     }
 
-    def show() {
-
-        def client = Client.findById(params.id)
-
-        if (client == null) {
-
-            response.status = 404
-
-            render(contentType: "application/json") {
-                error = "El cliente no existe"
-            }
-
-            return
-        }
-
-        render client as JSON
-
-    }
-
-    def existsName() {
-
-        def client = Client.findByName(params.id)
-
-        if (client == null) {
-            render(status: 200, contentType: "application/json") {
-                exists = "false"
-            }
-
-            return
-        }
-
-        render(status: 200, contentType: "application/json") {
-            exists = "true"
-        }
-    }
-
-    def existsSName() {
-
-        def client = Client.findByShort_name(params.id)
-
-        if (client == null) {
-            render(status: 200, contentType: "application/json") {
-                exists = "false"
-            }
-
-            return
-        }
-
-        render(status: 200, contentType: "application/json") {
-            exists = "true"
-        }
-    }
-
-    @Transactional
-    def create() {
-
-        def paramsJSON = request.JSON
-
-        def newClientParams = [
-                name: paramsJSON.get("name"),
-                short_name: paramsJSON.get("short_name"),
-                enabled: paramsJSON.get("enabled")
-        ]
-
-        def newClient = new Client(newClientParams)
-
-        if (!newClient.validate()) {
-
-            response.status = 422
-
-            render newClient.errors.fieldErrors as JSON
-
-            return
-        }
-
-        newClient.save flush: true
-
-        render newClient as JSON
-
-    }
-
-    @Transactional
-    def update() {
-
-        def paramsJSON = request.JSON
-
-        def clientToUpdate = Client.findById(paramsJSON.get("id"))
-
-        if (clientToUpdate == null) {
-
-            response.status = 404
-
-            render(contentType: "application/json") {
-                error = "El cliente no existe"
-            }
-
-            return
-        }
-
-        clientToUpdate.name = paramsJSON.get("name")
-        clientToUpdate.short_name = paramsJSON.get("short_name")
-        clientToUpdate.enabled = paramsJSON.get("enabled")
-
-        if (!clientToUpdate.validate()) {
-
-            response.status = 422
-
-            render clientToUpdate.errors.fieldErrors as JSON
-
-            return
-        }
-
-        clientToUpdate.save flush: true
-
-        render clientToUpdate as JSON
-
+    @Override
+    protected Object queryForResource(Serializable query) {
+        if (query.toString().isNumber())
+            Client.findById(query)
+        else
+            Client.findByName(query)
     }
 
 }
