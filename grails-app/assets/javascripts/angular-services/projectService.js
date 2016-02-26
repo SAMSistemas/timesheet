a16ed92445951d00b9a22b8667fce621176faed5
Santiago <santiago.pereztorre@gmail.com>
2016-02-26 10:31:26 -0300

    Update projectService to use new project api

--- grails-app/assets/javascripts/angular-services/projectService.js
+++ grails-app/assets/javascripts/angular-services/projectService.js
@@ -6,19 +6,19 @@ function projectService($http) {
     var vm = this;
 
     vm.getProjects = function(callbackSuccess, callbackFailure){
-        $http.get('/project/all').then(callbackSuccess, callbackFailure);
+        $http.get('/projects').then(callbackSuccess, callbackFailure);
     };
 
     vm.getEnabledProjectsByClient = function(clientName, callbackSuccess, callbackFailure){
-        $http.get('/project/allEnabledByClient/' + clientName).then(callbackSuccess, callbackFailure);
+        $http.get('/projects?enabled=true&client=' + clientName).then(callbackSuccess, callbackFailure);
     };
 
     vm.createProject = function(projectToCreate,callbackSuccess, callbackFailure){
-        $http.post('/project/create', projectToCreate).then(callbackSuccess, callbackFailure);
+        $http.post('/projects', projectToCreate).then(callbackSuccess, callbackFailure);
     };
 
     vm.updateProject = function(projectToEdit, callbackSuccess, callbackFailure){
-        $http.put('/project/update', projectToEdit).then(callbackSuccess, callbackFailure);
+        $http.put('/projects/' + projectToEdit.id, projectToEdit).then(callbackSuccess, callbackFailure);
     };
 
 }
