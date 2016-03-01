8fa26659ea96383480dfcb5237dd06dc6f484f4a
Santiago <santiago.pereztorre@gmail.com>
2016-03-01 10:12:44 -0300

    Little refactor 3

--- grails-app/assets/javascripts/angular-controllers/ts-table.js
+++ grails-app/assets/javascripts/angular-controllers/ts-table.js
@@ -29,32 +29,6 @@
             return lowerStr.indexOf(expected.toLowerCase()) === 0;
         };
 
-
-        /* Filter clients by enabled */
-
-        //vm.filter = function (clients) {
-        //    var result = {};
-        //    angular.forEach(clients, function (client, key) {
-        //        if (!client.enabled) {
-        //            return;
-        //        }
-        //        result[key] = client;
-        //    });
-        //    return result;
-        //};
-        //
-        //vm.isEnabled = function (client_name) {
-        //
-        //    vm.enabledClients.forEach(function (client) {
-        //        console.log(client_name);
-        //        console.log(client.name);
-        //        if (!client.name.localeCompare(client_name)) {
-        //            return true;
-        //        }
-        //    });
-        //    return false;
-        //};
-
     }
 
 })();
